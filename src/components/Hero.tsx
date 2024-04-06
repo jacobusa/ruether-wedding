4cbb6d368b5a6617cb3182b01b2846db00a3afd0
Kobus <jacobusbad@gmail.com>
2024-04-06 12:10:47 -0700

    Hero and story responsiveness

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,26 +8,26 @@ export const Hero: React.FC = () => {
         <HeroImage />
       </div>
       <header className="flex h-screen flex-col z-10 content-center justify-center">
-        <h2 className="uppercase text-xs tracking-widest text-gray-300 self-center">
-          we are getting married
+        <h2 className="uppercase text-xs  tracking-widest text-gray-300 self-center">
+          we are getting married!
         </h2>
-        <h1 className="tracking-tight self-center text-gray-200 my-10 md:text-9xl lg:text-9xl font-brush">
+        <h1 className="tracking-tight self-center text-gray-200 my-10 text-[14vw] lg:text-9xl font-brush">
           Kurt <strong className="font-medium mx-2">&</strong> Cecile
         </h1>
         <div className="flex justify-center content-center">
           <div className="bg-orange-200/35 w-full h-[1px] self-center" />
-          <h3 className="uppercase text-gray-300 self-center w-full text-center tracking-wide">
+          <h3 className="uppercase text-gray-300 self-center whitespace-nowrap w-full text-center tracking-wide">
             Save the Date
           </h3>
           <div className="bg-orange-200/35 w-full h-[1px] self-center" />
         </div>
-        <p className="self-center mt-10 font-extralight text-gray-300 text-3xl py-4 px-8">
+        <p className="self-center mt-10 font-extralight text-gray-300 text-xl md:text-3xl py-4 px-8">
           August 28th 2025
         </p>
         <a
           href="google.com"
           target="_blank"
-          className="self-center mt-10 font-thin text-gray-300 cursor-pointer text-3xl border-2 border-orange-200/35 py-4 px-8 z-50"
+          className="self-center mt-10 font-thin text-gray-300 cursor-pointer text-xl md:text-3xl border-2 border-orange-200/35 py-4 px-8 z-50"
         >
           See Our Registry
         </a>
--- src/components/OurStory.tsx
+++ src/components/OurStory.tsx
@@ -5,26 +5,28 @@ import Image from "next/image";
 
 export const OurStory: React.FC = () => {
   return (
-    <section className="bg-gray-100 min-h-[110vh]">
+    <section className="bg-gray-100 p-4 pb-44">
       <div className="w-full pt-28 flex justify-center flex-col">
         <Image
           alt="wedding flower"
           className="w-24 h-11 self-center"
           src={flowerImage}
         />
-        <h4 className="text-gray-800 text-5xl font-light text-center">
+        <h4 className="text-gray-800 text-2xl md:text-5xl font-light text-center">
           Our Story
         </h4>
-        <p className="max-w-[1000px] text-gray-500 text-center font-light py-4 self-center tracking-wide">
+        <p className="max-w-[700px] text-gray-500 text-center  font-light py-4 self-center text-sm md:text-base tracking-wide">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur
           repellendus totam modi voluptas cum commodi aspernatur ut minima a,
           excepturi odit vel, fuga, suscipit dolores possimus quam reprehenderit
           voluptates dolor. Perferendis totam eius aut molestias
         </p>
-        <div className="mt-10 self-center w-full flex space-x-10 px-7 max-w-[1300px]">
-          <div>
-            <h5 className="text-gray-800 text-4xl mb-7">Cecile Badenhorst</h5>
-            <p className="text-gray-500 font-light text-sm leading-6 tracking-wide text-right">
+        <div className="mt-10 self-center w-full flex flex-col md:flex-row  max-w-[1000px]">
+          <div className="mb-4">
+            <h5 className="text-gray-800 lg:whitespace-nowrap text-2xl md:text-4xl mb-7">
+              Cecile Badenhorst
+            </h5>
+            <p className="text-gray-500 max-w-[600px]  font-light text-sm leading-6 tracking-wide md:text-right">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque,
               doloremque earum fugit voluptatem repudiandae unde necessitatibus.
               Recusandae, itaque praesentium voluptatibus modi veritatis
@@ -32,16 +34,26 @@ export const OurStory: React.FC = () => {
               eaque.
             </p>
           </div>
-          <Image alt="Cecile and kurt oval photo" src={ovalImage} />
-          <div className="mt-32">
-            <h5 className="text-gray-800 text-4xl mb-7">Kurt Ruether</h5>
-            <p className="text-gray-500 font-light text-sm leading-6 tracking-wide">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque,
-              doloremque earum fugit voluptatem repudiandae unde necessitatibus.
-              Recusandae, itaque praesentium voluptatibus modi veritatis
-              reiciendis ea ducimus, consectetur delectus, excepturi omnis
-              eaque.
-            </p>
+          <div className="flex justify-center content-center w-full">
+            <Image
+              alt="Cecile and kurt oval photo"
+              src={ovalImage}
+              className="w-[400px] shrink-0"
+            />
+          </div>
+          <div className="md:mt-32">
+            <h5 className="text-gray-800 text-2xl text-right md:text-left md:text-4xl mb-7">
+              Kurt Ruether
+            </h5>
+            <div className="flex justify-end">
+              <p className="text-gray-500 max-w-[600px] font-light text-sm leading-6 text-right md:text-left tracking-wide">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Cumque, doloremque earum fugit voluptatem repudiandae unde
+                necessitatibus. Recusandae, itaque praesentium voluptatibus modi
+                veritatis reiciendis ea ducimus, consectetur delectus, excepturi
+                omnis eaque.
+              </p>
+            </div>
           </div>
         </div>
       </div>
